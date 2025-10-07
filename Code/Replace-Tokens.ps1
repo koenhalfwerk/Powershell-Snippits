15f2015d661b653efe1eda81a63371935c3f2414
Koen Halfwerk <kha096@uwv.nl>
2025-10-07 10:55:22 +0200

    adds replace tokens

--- Code/Replace-Tokens.ps1
+++ Code/Replace-Tokens.ps1
@@ -1,4 +1,5 @@
-<#
+function Replace-Token {
+    <#
 .SYNOPSIS
     Replaces tokens in a file
 .DESCRIPTION
@@ -18,71 +19,77 @@
 .NOTES
     General notes 
 #>
-[CmdletBinding(DefaultParameterSetName = 'Array', 
-    SupportsShouldProcess = $true,
-    PositionalBinding = $false,
-    HelpUri = 'https://dev.azure.com/UICTS-OPS/UICTS-Docs/_wiki/wikis/UICTS-Docs/279/README')]
-Param
-(
-    [Parameter(Mandatory = $false, ParameterSetName = 'Array')] $TokenArray,
-    [Parameter(Mandatory = $false, ParameterSetName = 'String')] [string[]] $TokenString,
-    [Parameter(Mandatory = $true, ParameterSetName = 'String')] [string[]] $TokenValue,
-    [Parameter(Mandatory = $true)] [ValidateScript( { test-path $_ })] [string] $File,
-    [Parameter(Mandatory = $true)] [string] $VariableName,
-    # Used when the $file is used multiple times in the same stages
-    [Parameter(Mandatory = $false)] [string] $FilePrefix
-)
-#Requires -psedition core, desktop
-Begin {
-    if (Test-Path $File) {
-        $content = Get-Content -Path $File -raw
-    }
-    else {
-        Write-Error ('File {0} not found from path {1}' -f $File, (Get-Item .\ | Select-Object -ExpandProperty fullname))
-        break
-    }
-    $fileObj = Get-Item $File
-    $fileParent = Split-Path -Path $fileObj -Parent
-    Remove-Variable -Name OutputFile -ErrorAction SilentlyContinue
-    if ($TokenString) {
-        $TokenArray = @{}
-        for ($i = 0 ; $i -lt $Tokenstring.Count; $i++) {
-            $TokenArray.Add($TokenString[$i], $TokenValue[$i])
+    [CmdletBinding(DefaultParameterSetName = 'Array', 
+        SupportsShouldProcess = $true,
+        PositionalBinding = $false)]
+    Param
+    (
+        [Parameter(Mandatory = $false, ParameterSetName = 'Array')] $TokenArray,
+        [Parameter(Mandatory = $false, ParameterSetName = 'String')] [string[]] $TokenString,
+        [Parameter(Mandatory = $true, ParameterSetName = 'String')] [string[]] $TokenValue,
+        [Parameter(Mandatory = $true)] [ValidateScript( { test-path $_ })] [string] $File,
+        [Parameter(Mandatory = $true)] [string] $VariableName,
+        # Used when the $file is used multiple times in the same stages
+        [Parameter(Mandatory = $false)] [string] $FilePrefix,
+        [Parameter(Mandatory = $false)] [switch] $AsObject
+    )
+    #Requires -psedition core, desktop
+    Begin {
+        if (Test-Path $File) {
+            $content = Get-Content -Path $File -raw
         }
-    }
-
-    $outputFile = Join-Path -Path $fileParent -ChildPath ($FilePrefix + $fileObj.Name) -Resolve -ErrorAction SilentlyContinue
-
-    if (!$outputFile) {
-        foreach ($number in (0..100)) {
-            $outputFile = Join-Path -Path $fileParent -ChildPath ([string]$number + $fileObj.Name)
-            if ((Test-Path -Path $outputFile)) {
-                Write-Output "$outputFile already exists"
-            }
-            else {
-                Write-Output "Using $outputFile"
-                break
+        else {
+            Write-Error ('File {0} not found from path {1}' -f $File, (Get-Item .\ | Select-Object -ExpandProperty fullname))
+            break
+        }
+        $fileObj = Get-Item $File
+        $fileParent = Split-Path -Path $fileObj -Parent
+        Remove-Variable -Name OutputFile -ErrorAction SilentlyContinue
+        if ($TokenString) {
+            $TokenArray = @{}
+            for ($i = 0 ; $i -lt $Tokenstring.Count; $i++) {
+                $TokenArray.Add($TokenString[$i], $TokenValue[$i])
             }
         }
-    }
-}
-Process {
-    foreach ($token in ([string[]]$TokenArray.keys)) {
-        if ($psCmdlet.ShouldProcess($File, ('Replace Token {0} with value "{1}"' -f [string]$token, ($TokenArray[$token] | Out-String)))) {
-            if (($content.indexof($token)) -eq -1) {
-                Write-Warning ('Token {1} not found in file {0}' -f $File, $token)
-            }
-            else {
-                Write-Verbose ('Replacing {0} with {1} in file {2}' -f $token, $TokenArray[$token], $File | Out-String)
-                $content = $content.Replace($token, $TokenArray["$token"])
+
+        $outputFile = Join-Path -Path $fileParent -ChildPath ($FilePrefix + $fileObj.Name) -Resolve -ErrorAction SilentlyContinue
+
+        if (!$outputFile) {
+            foreach ($number in (0..100)) {
+                $outputFile = Join-Path -Path $fileParent -ChildPath ([string]$number + $fileObj.Name)
+                if ((Test-Path -Path $outputFile)) {
+                    Write-Output "$outputFile already exists"
+                }
+                else {
+                    Write-Output "Using $outputFile"
+                    break
+                }
             }
         }
     }
-}
-End {
-    $content | Set-Content -Path $outputFile -Verbose
+    Process {
+        foreach ($token in ([string[]]$TokenArray.keys)) {
+            if ($psCmdlet.ShouldProcess($File, ('Replace Token {0} with value "{1}"' -f [string]$token, ($TokenArray[$token] | Out-String)))) {
+                if (($content.indexof($token)) -eq -1) {
+                    Write-Warning ('Token {1} not found in file {0}' -f $File, $token)
+                }
+                else {
+                    Write-Verbose ('Replacing {0} with {1} in file {2}' -f $token, $TokenArray[$token], $File | Out-String)
+                    $content = $content.Replace($token, $TokenArray["$token"])
+                }
+            }
+        }
+    }
+    End {
+        if ($AsObject) {
+            return $content
+        }
+        else {
+            $content | Set-Content -Path $outputFile -Verbose
 
-    Write-Output "Writing property $VariableName value $($outputFile)"
-    Write-Output "##vso[task.setvariable variable=$VariableName]$($outputFile)"
-    Write-Output "##vso[task.setvariable variable=$VariableName;isOutput=true]$($outputFile)"
+            Write-Output "Writing property $VariableName value $($outputFile)"
+            Write-Output "##vso[task.setvariable variable=$VariableName]$($outputFile)"
+            Write-Output "##vso[task.setvariable variable=$VariableName;isOutput=true]$($outputFile)"
+        }
+    }
 }
